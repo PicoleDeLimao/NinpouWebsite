a3a2c612a962fbf0f2e48f37a1208aff5c6cd1e2
Abner <abner.math.c@gmail.com>
2018-03-10 20:43:38 -0300

    kappa again

--- routes/stats.js
+++ routes/stats.js
@@ -134,7 +134,7 @@ function getSlotId(playerId) {
 
 function AgrestiCoullLower(n, k) {
 	//float conf = 0.05;  // 95% confidence interval
-	var kappa = 3;//2.24140273; // In general, kappa = ierfc(conf/2)*sqrt(2)
+	var kappa = 5;//2.24140273; // In general, kappa = ierfc(conf/2)*sqrt(2)
 	var kest=k+Math.pow(kappa,2)/2;
 	var nest=n+Math.pow(kappa,2);
 	var pest=kest/nest;
