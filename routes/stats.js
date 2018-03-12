a0437ed4590bee02e99b0363cf0848800769bd51
Abner <abner.math.c@gmail.com>
2018-03-11 23:24:11 -0300

    new formula

--- routes/stats.js
+++ routes/stats.js
@@ -137,8 +137,8 @@ function AgrestiCoullLower(n, k) {
 	//float conf = 0.05;  // 95% confidence interval
 	var kappa = 10;//2.24140273; // In general, kappa = ierfc(conf/2)*sqrt(2)
 	var kest=k+Math.pow(kappa,2)/2;
-	var nest=n+Math.pow(kappa,2); 
-	var pest=kest/nest; 
+	var nest=n+Math.pow(kappa,2);
+	var pest=kest/nest;
 	var radius=kappa*Math.sqrt(pest*(1-pest)/nest);
 	return Math.max(0,pest-radius); // Lower bound
 	// Upper bound is min(1,pest+radius)
@@ -146,7 +146,7 @@ function AgrestiCoullLower(n, k) {
 
 function calculateScore(stat) {
 	stat.chanceWin = stat.chanceWin || AgrestiCoullLower(stat.games, stat.wins);
-	var score = (stat.kills / stat.games * 10 - stat.deaths / stat.games * 5 + stat.assists / stat.games * 2) * (stat.gpm / stat.games) * stat.chanceWin * 100;  
+	var score = (stat.kills / stat.games * 10 - stat.deaths / stat.games * 5 + stat.assists / stat.games * 2) * (stat.gpm * 100 / stat.games) * stat.chanceWin;   
 	if (isNaN(score)) return 0;  
 	return score; 
 }; 
