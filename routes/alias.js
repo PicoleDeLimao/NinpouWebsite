8a016b0cf7fe1c4a8a02f17dc1a751cc503ff879
Abner Matheus <abner.math.c@gmail.com>
2018-04-05 18:10:39 -0300

    fixed add alias

--- routes/alias.js
+++ routes/alias.js
@@ -56,7 +56,7 @@ router.get('/:alias', function(req, res) {
 });
 
 router.put('/:username/:alias', function(req, res) {
-	if (!req.param.username || !req.param.alias) return res.status(400).json({ error: 'Alias not found.' });
+	if (!req.params.username || !req.params.alias) return res.status(400).json({ error: 'Alias not found.' });
 	Alias.findOne({ alias: req.params.alias.toLowerCase() }, function(err, alias) {
 		if (err) return res.status(500).json({ error: err });
 		else if (alias) {
@@ -94,7 +94,7 @@ router.put('/:username/:alias', function(req, res) {
 });
  
 router.delete('/:username/:alias', function(req, res) {
-	if (!req.param.username || !req.param.alias) return res.status(400).json({ error: 'Alias not found.' });
+	if (!req.params.username || !req.params.alias) return res.status(400).json({ error: 'Alias not found.' });
 	Alias.findOne({ alias: req.params.alias.toLowerCase() }, function(err, alias) {
 		if (err) return res.status(500).json({ error: err });
 		else if (!alias) return res.status(404).json({ error: 'Alias not found.' });
@@ -102,10 +102,10 @@ router.delete('/:username/:alias', function(req, res) {
 			if (err) return res.status(500).json({ error: err });
 			for (var i = 0; i < alias.alias.length; i++) {
 				if (alias.alias[i].toLowerCase() == req.params.alias.toLowerCase()) {
-					alias.alias.splice(i, 1);
+					alias.alias.splice(i, 1); 
 					alias.save(function(err) {
 						if (err) return res.status(500).json({ error: err });
-						Stat.findOne({ username: req.param.alias.toLowerCase() }, function(err, stat) {
+						Stat.findOne({ username: req.params.alias.toLowerCase() }, function(err, stat) {
 							if (!stat) return res.status(200).json(alias);
 							stat.alias = stat.username;
 							stat.save(function(err) {
