9c6e81a7fb86b9d49a98b76325fce2fee5a939ea
Abner Araujo <abner.mathc@gmail.com>
2018-12-26 20:38:17 -0200

    fixed usernames

--- bot/commands/getplayername.js
+++ bot/commands/getplayername.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var http = require('http');
+
 module.exports = function(ev, name, callback, hideRole) {
 	if (!name) return callback(null, null);
 	if (!isNaN(parseInt(name))) {
@@ -18,7 +20,26 @@ module.exports = function(ev, name, callback, hideRole) {
 				}); 
 				return callback(null, member.displayName + (hideRole ? '' : ' (' + roleName.replace('ū', 'uu').replace('ō', 'ou') + ')' )); 
 			}).catch(function(err) { 
-				return callback(null, name);
+				http.get({ host: '127.0.0.1', port: (process.env.PORT || 8080), path: '/alias/' + name, headers: { 'Content-Type': 'application/json', 'Content-Length': '0' } }, function(res) {
+					var body = '';
+					res.on('data', function(chunk) {
+						body += chunk;
+					});
+					res.on('end', function() { 
+						if (res.statusCode != 200) { 
+							console.err(body);
+							return callback(null, user.username + ' (LEFT)');
+						} else {
+							try { 
+								var data = JSON.parse(body); 
+								return callback(null, data.alias[0] + ' (LEFT)');
+							} catch (err) {
+								console.err(err);
+								return callback(null, user.username + ' (LEFT)');
+							}
+						}
+					});
+				}).end();
 			});
 		}).catch(function(err) {
 			return callback(null, name);
