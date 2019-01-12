5d6a908f6e60d8890ef4b5d3c181c0646d49803d
Abner Araujo <abner.mathc@gmail.com>
2019-01-12 20:27:33 -0200

    update version

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use('/items', require('./routes/items'));
 app.use('/heroes', require('./routes/heroes'));  
 
 app.get('/latest', function(req, res) {
-	res.redirect('https://drive.google.com/file/d/1EGaP5nhF_5jWSWCLFYX0tmRKhq5YUflV/view?usp=sharing');
+	res.redirect('https://drive.google.com/file/d/10c2x9NZdNJkpGjNoRYwC5woU_-Fv1PRq/view?usp=sharing');
 }); 
    
 app.get('/changelog', function(req, res) {
