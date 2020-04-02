c8cbc358e61a051798b6b23c89a50460994634d4
Abner Araujo <abner.mathc@gmail.com>
2020-04-01 21:48:49 -0300

    add update

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log('Listening on port ' + port + '...');
 	setInterval(function() {
+		Game.updateMany({}, { $set: { ranked: true }});
 		Stat.find({ }, function(err, stats) {
 			if (err) {
 				console.error(err);
