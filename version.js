5165f88d7d180b03ccb9ea6ab88bc4a0339c79de
Abner Araujo <abner.rjo@gmail.com>
2020-05-31 23:40:09 -0300

    new version

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    version: '3.6',
-    download_link: 'https://drive.google.com/file/d/1AyYZN7imwE7-TyoLAu2B0WNt1j8gcvCj/view?usp=sharing',
-    changelog: 'http://www.narutoninpou.com/#/forum/threads/5ed33c295959750015699773'
+    version: '3.7',
+    download_link: 'https://drive.google.com/file/d/1EB18kb3uS9hBYvtQBnV2WSVvKvN498eU/view?usp=sharing',
+    changelog: 'http://www.narutoninpou.com/#/forum/threads/5ed469e1ec71f400159e4660'
 }
