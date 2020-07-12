7f7b02b2f0c84c66856147e62562e88a7b4a796a
Dev4nt <thevirustroy@gmail.com>
2020-07-12 22:36:33 +0300

    Update version.js

--- version.js
+++ version.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     version: '4.6',
-    download_link: 'https://drive.google.com/file/d/13tt5GaC2C0895yAi66AE2NPlSk0zskfQ/view?usp=sharing',
+    download_link: 'https://drive.google.com/file/d/1SsWR4_cOxPvcqRTaDAOpO6HlHcvajf3q/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/5f0b5c367206020015b35457'
 }
