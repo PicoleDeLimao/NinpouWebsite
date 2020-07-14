ea3520ab10abd44b9a1265b7b3f5a1c6473bf914
Abner Arajo <abner.rjo@gmail.com>
2020-07-14 19:44:07 -0300

    adding villages

---
+++ routes/villages.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var Alias = require('../models/Alias');
+var Stat = require('../models/Stat');
+var StatCalculator = require('./statcalculator');
+
+
+router.get('/:name', function(req, res) {
+    var name = req.params.name;
+    if (name !== "shinobi alliance" && name !== "otogakure" && name !== "akatsuki") {
+        return res.status(400).json({ error: 'Village not found.' });
+    }
+    var affiliations;
+    if (name === "shinobi") {
+        affiliations = ['konohagakure', 'sunagakure', 'kumogakure', 'iwagakure', 'kirigakure'];
+    } else if (name === "oto") {
+        affiliations = ['otogakure'];
+    } else {
+        affiliations = ['akatsuki'];
+    }
+    StatCalculator.getAllPlayersRanking(function(err, stats) {
+        if (err) return res.status(400).json({ error: err }); 
+        Alias.find({ affiliation: { $or: affiliations } }, function(err, aliases) {
+            if (err) return res.status(400).json({ error: err }); 
+            var aliasesId = {};
+            for (var i = 0; i < aliases.length; i++) {
+                for (var j = 0; j < aliases[i].alias.length; j++) {
+                    aliasesId[aliases[i].alias[j]] = aliases[i];
+                }
+            }
+            var hierarchy = { 'kage': [], 'anbu': [], 'jounin': [], 'tokubetsu jounin': [], 'chunnin': [], 'genin': [] };
+            for (var i = 0; i < stats.length; i++) {
+                if (aliasesId.hasOwnProperty(stats[i]._id)) {
+                    hierarchy[aliasesId[stats[i]._id].rank].push(stats[i]._id);
+                }
+            }
+            return res.json(hierarchy);
+        });
+    });
+});
