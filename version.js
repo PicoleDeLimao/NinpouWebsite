d49e05b3ee420f9a1537f251cc4ee41c890db611
Dev4nt <thevirustroy@gmail.com>
2020-07-22 01:10:28 +0300

    Update version.js

--- version.js
+++ version.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     version: '5.1',
-    download_link: 'https://drive.google.com/file/d/1EekOMA-fB62qyMzEFH8IqKu9gxg6qJdb/view?usp=sharing',
+    download_link: 'https://drive.google.com/file/d/1gVSCyvCxkzurLHLVkkwGICxQ-jXRZihH/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/5f172cc8dd8021001503e3f5'
 }
