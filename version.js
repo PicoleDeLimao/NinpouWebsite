96a27251ce4accc3819b60c011dc7f973efd13f0
Abner Araujo <abner.math.c@gmail.com>
2020-09-07 18:45:27 -0300

    Update version.js

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    version: '5.4',
-    download_link: 'https://drive.google.com/file/d/1Ri1DxhY-qzxoh0oREz5EgjPE4_RL7V_N/view?usp=sharing',
-    changelog: 'http://www.narutoninpou.com/#/forum/threads/5f543feb0471290017dbcc12'
+    version: '5.5',
+    download_link: 'https://drive.google.com/file/d/1utCuu6ixyGjucarwffWUb-vcilMV12LK/view?usp=sharing',
+    changelog: 'http://www.narutoninpou.com/#/forum/threads/5f56a8e017216b0017164b55'
 }
