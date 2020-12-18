3ec2c1ad4ecb3326e011672b404ae6ca8cba7bf7
Abner Araujo <abner.rjo@gmail.com>
2020-12-18 14:07:18 -0300

    update download link

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    version: '5.1',
-    download_link: 'https://drive.google.com/file/d/1x3FJWcA8cvdNdKNHxM4_pivhMWzGmBXv/view?usp=sharing',
+    version: '6.1',
+    download_link: 'https://drive.google.com/file/d/1EU4S6Wzx5TzTSSO4qNblV2WSN2ywUciB/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/5f14d5da5277ae00151d22a3'
 }
