0da896dd807e36e579acc4ffc2ce1e37ce9341bc
Abner Araujo <abner.rjo@gmail.com>
2021-01-03 15:22:01 -0300

    new version

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    version: '6.1',
-    download_link: 'https://drive.google.com/file/d/1EU4S6Wzx5TzTSSO4qNblV2WSN2ywUciB/view?usp=sharing',
+    version: '6.4',
+    download_link: 'https://drive.google.com/file/d/1Ea12WB8wh4dwgDyE6gbwE_5pb9F0Og-_/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/5f14d5da5277ae00151d22a3'
 }
