11eb62fb8b661233ec0c89e3895fa6a3c7cd08bb
RiP-MaN <67857505+RiP-MaN@users.noreply.github.com>
2021-02-15 19:01:39 +0100

    Update version.js

--- version.js
+++ version.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     version: '6.8',
-    download_link: 'https://drive.google.com/file/d/1I025De7RchwlDldBOUaNBOpPJV5_-yuY/view?usp=sharing',
+    download_link: 'https://drive.google.com/file/d/1hV4h-NPbgQgmP9alPZo74n-2eQGb-hUR/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/602aa9d385a02400177afdc8'
 }
