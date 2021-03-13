f753e6cfe47ac6f41d96e6bb925725c3ddd2254e
Abner Arajo <abner.rjo@gmail.com>
2021-03-13 18:01:43 -0300

    fix

--- models/Event.js
+++ models/Event.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var eventSchema = mongoose.Schema({
 	id: { type: String, required: true, unique: true, index: true },
 	createdAt: { type: Date, default: Date.now },
-    name: { type: string, required: true, unique: true, index: true }
+    name: { type: String, required: true, unique: true, index: true }
 });
 
 var Event = mongoose.model('Event', eventSchema);
