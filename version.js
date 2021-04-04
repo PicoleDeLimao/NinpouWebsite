c5fe777fb6f7adf720399ca9d1415939bad0a92f
MetalfOxXer <66499312+MetalfOxXer@users.noreply.github.com>
2021-04-04 23:25:11 +0200

    Update version.js

--- version.js
+++ version.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     version: '6.9',
-    download_link: 'https://drive.google.com/file/d/1KIYyP5GB7snP8UmRplAJFc4-A5mDKjYy/view?usp=sharing',
+    download_link: 'https://drive.google.com/file/d/1VqtlEE-SuXyIv9KPfssxsHefr5eX8ytk/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/602aa9d385a02400177afdc8'
 }
